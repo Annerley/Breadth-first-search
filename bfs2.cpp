56b310000f4d254ec28cda3d4f7d24c3609d078c
Дарья Лапутина <laputina975@gmail.com>
2020-09-05 23:44:07 +0300

    transfered to function, comment, doxygen

--- bfs2.cpp
+++ bfs2.cpp
@@ -4,14 +4,17 @@
 
 using namespace std;
 
+int breadth_first_search(int start, int end, int** adjacency_matrix, int N);
 
 int main()
 {
+	//реализован ввод из файла
 	ofstream fout;
 	fout.open("OUTPUT.txt");
 	ifstream fin;
 	fin.open("INPUT.txt");
-	//ввод вершин
+
+	//ввод размерности
 	int N;
 	fin >> N;
 
@@ -27,13 +30,36 @@ int main()
 		}
 	}
 
-	
+	//ввод вершин
 	int start, end;
 	fin >> start >> end;
 
+	//вывод в файл
+	fout << breadth_first_search(start, end, adjacency_matrix, N);
+
+	fout.close();
+	fin.close();
+	return 0;
+}
+/*
+	\brief Находит минимальное расстояние от двух вершин по алгоритму BFS
+	Работает только с корректными данными, то есть введенные вершины существуют в графе
+
+	\param[in] start Начальная вершина
+	\param[in] end Конечная вершина
+	\param[in] adjacency_matrix Матрица смежности
+	\param[in] N Размерность матрицы
+
+	\return Возвращает минимальное расстояние в виде типа int
+
+*/
+int breadth_first_search(int start, int end, int** adjacency_matrix, int N)
+{
 	queue <int> q;
 
 	int* lenght = new int[N];
+
+	//задаем стартовые значения
 	for (int i = 0; i < N; i++)
 	{
 		lenght[i] = -1;
@@ -41,31 +67,28 @@ int main()
 	q.push(start - 1);
 
 	lenght[start - 1] = 0;
+
 	while (!q.empty())
 	{
 		int i = q.front();
 		q.pop();
-		
+
 		for (int j = 0; j < N; j++)
 		{
-			
 			if (adjacency_matrix[i][j] == 1)
 			{
- 				if (lenght[j] == -1 && i!=j)
+				if (lenght[j] == -1 && i != j)
 				{
 					q.push(j);
-					lenght[q.back()] = lenght[i]+1;
+					lenght[q.back()] = lenght[i] + 1;
 				}
 
 			}
 		}
 	}
 	if (start == end) lenght[end - 1] = 0;
-	
-	fout << lenght[end - 1];
-	fout.close();
-	fin.close();
-	return 0;
+
+	return lenght[end - 1];
 }
 
 
